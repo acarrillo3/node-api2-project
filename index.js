e5f711e41576bafe8a4fcbb2ddd3f34012e14e60
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-02-11 20:07:04 -0500

    npm i and express, file structure, and set up index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+//const apiRouter = require("./api/api-router.js");
+
+const server = express();
+
+server.use(express.json()); // needed to parse JSON from the body
+
+//for URLs beginning with /api
+//server.use("/api", apiRouter);
+
+server.get("/", (req, res) => {
+    res.send(`
+      <h2>Building RESTful APIs with Express</h>
+      <p>Welcome to the Blog</p>
+    `);
+  });
+  
+  const port = 5000;
+  server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+  });
+//after this make nodes.md file
